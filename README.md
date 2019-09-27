0c43ce875706d7a6828e3ea927dc211a7c4aab37
Filipe Silva <filipematossilva@gmail.com>
2019-09-27 14:44:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Repro for https://github.com/terser/terser/issues/470
+
+```
+git clone https://github.com/filipesilva/terser-4.3.3-size-regression
+cd terser-4.3.3-size-regression
+npm i
+npm test
+```
+
+You should see the following output:
+```
+input is 1454.80 kB at ./input.js
+terser@4.3.2 output is 13.58 kB at ./output-4.3.2.js
+terser@4.3.3 output is 124.01 kB at ./output-4.3.3.js
+```
---
+++ terser-test.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const Terser_4_3_2 = require('./terser-4.3.2');
+const Terser_4_3_3 = require('./terser-4.3.3');
+
+const input = fs.readFileSync('./input.js', 'utf-8');
+console.log(`input is ${(input.length / 1024).toFixed(2)} kB at ./input.js`);
+const terserOptions = {
+  warnings: false,
+  safari10: true,
+  output: { ecma: 6, comments: false, webkit: true },
+  compress: {
+    ecma: 6,
+    pure_getters: true,
+    passes: 3,
+    global_defs: { ngDevMode: false, ngI18nClosureMode: false }
+  },
+  mangle: true
+};
+
+const result_4_3_2 = Terser_4_3_2.minify(input, terserOptions);
+if (result_4_3_2.error) {
+  throw new Error(result_4_3_2.error);
+} else {
+  fs.writeFileSync('./output-4.3.2.js', result_4_3_2.code);
+  console.log(`terser@4.3.2 output is ${(result_4_3_2.code.length / 1024).toFixed(2)} kB at ./output-4.3.2.js`);
+}
+
+const result_4_3_3 = Terser_4_3_3.minify(input, terserOptions);
+if (result_4_3_3.error) {
+  throw new Error(result_4_3_3.error);
+} else {
+  fs.writeFileSync('./output-4.3.3.js', result_4_3_3.code);
+  console.log(`terser@4.3.3 output is ${(result_4_3_3.code.length / 1024).toFixed(2)} kB at ./output-4.3.3.js`);
+}
+
